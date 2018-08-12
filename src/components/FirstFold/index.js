e2afc81777495dc4df699df85cd00954b18c5c51
Jaydeep Solanki <jaydp17@gmail.com>
2018-08-12 17:54:53 +0530

    add css magic to first fold

--- src/components/FirstFold/index.js
+++ src/components/FirstFold/index.js
@@ -7,6 +7,8 @@ import PhoneSlider from './PhoneSlider';
 
 const BaseLeftElement = styled.div`
   text-align: right;
+  padding-left: 15%;
+  padding-right: 5%;
 `;
 
 const GridContainer = styled.div`
@@ -17,6 +19,13 @@ const GridContainer = styled.div`
 const PhoneElement = styled.div`
   grid-row: 1 / 4;
   grid-column: 2;
+  text-align: center;
+`;
+
+const TagLine = styled.p`
+  font-size: 1.5em;
+  font-weight: 400;
+  color: #767d89;
 `;
 
 export default () => {
@@ -29,13 +38,13 @@ export default () => {
         <PhoneSlider />
       </PhoneElement>
       <BaseLeftElement>
-        <p>
+        <TagLine>
           With YoloBots you can get stuff done fast without the need to have hundreds of apps for
           every little task.
-        </p>
+        </TagLine>
       </BaseLeftElement>
       <BaseLeftElement>
-        <img src={googlePlayBadge} width="250" alt="get on google play button"/>
+        <img src={googlePlayBadge} width="250" alt="get on google play button" />
       </BaseLeftElement>
     </GridContainer>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import FirstFold from '../components/FirstFold';
+import styled from 'styled-components';
+
+const MeaningFulContent = styled.div`
+  margin-top: 80px;
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto;
+  @media (min-width: 768px) {
+    width: 750px;
+  }
+  @media (min-width: 992px) {
+    width: 970px;
+  }
+  @media (min-width: 1200px) {
+    width: 1170px;
+  }
+`;
 
 export default () => {
   return (
     <div>
-      <FirstFold />
+      <MeaningFulContent>
+        <FirstFold />
+      </MeaningFulContent>
     </div>
   );
 };
